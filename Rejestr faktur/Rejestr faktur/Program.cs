6f7bc8d48046327b8e23ce756ecf2152951fa7b9
Rafał Dąbrowski <ravals887@gmail.com>
2017-04-14 00:51:28 +0200

    a

--- Rejestr faktur/Rejestr faktur/Program.cs
+++ Rejestr faktur/Rejestr faktur/Program.cs
@@ -16,7 +16,7 @@ namespace Rejestr_faktur
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Rejestr());
         }
     }
 }
--- Rejestr faktur/Rejestr faktur/Rejestr.cs
+++ Rejestr faktur/Rejestr faktur/Rejestr.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Rejestr_faktur
 {
-    public partial class Form1 : Form
+    public partial class Rejestr : Form
     {
-        public Form1()
+        public Rejestr()
         {
             InitializeComponent();
         }
